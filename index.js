1a895766fac2e8cfaed29f0dae3bb548ec9f7690
Luis J Camargo <lsjcp@yahoo.com>
2021-03-31 01:58:01 -0600

    automated last changes commit

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ class IBMQJSApi {
         'code': (id) => `/Codes/${id}`,
         'jobsStatus': '/Jobs/status',
         'circuit': '/qcircuit',
-        'version': '/version'
+        'version': '/version',
       },
     };
     this.config = {
@@ -94,13 +94,18 @@ class IBMQJSApi {
     return this.post(base, path, content)
   }
 
+  apost2(base, path, content) {
+    content = content || {}
+    content['access_token'] = this.session.id || 'None'
+    return this.post(base, path, content)
+  }
+
   login() {
     this.log(`logging in`);
     return rp(this.apost(this.auth.url, this.auth.endpoints.login))
       .then(parsed => {
           this.log(`login success!`, parsed);
           this.session = parsed;
-          done(parsed);
           return parsed;
       })
       .catch(err => this.warn(`login error! ${err}`));
@@ -138,6 +143,17 @@ class IBMQJSApi {
         return parsed;
       })
       .catch(err => this.warn(`get detail of ${kind} "${name}" error! ${err}`));
+  }
+
+  submit(kind, path, name, done) {
+    this.log(`submit ${kind} "${name}"`);
+    return rp(this.apost2(this.api.url, path))
+      .then(parsed => {
+        this.log(`get results of submission ${kind} "${name}" success!`, parsed);
+        done(parsed);
+        return parsed;
+      })
+      .catch(err => this.warn(`error while submitting ${kind} "${name}"! ${err}`));
   }
 
   userData(done) {
@@ -220,6 +236,10 @@ class IBMQJSApi {
     return this.detail("version", this.api.endpoints.version, null, done);
   };
 
+  submitJob(job, done) {
+    return this.submit("job", this.api.endpoints.jobs, job, done);
+  };
+
 };
 
 module.exports = IBMQJSApi;
@@ -247,31 +267,221 @@ api
     console.log("got job");
     return api.code(job.codeId, ()=>{});
   })
-  .then(code => console.log(code));
-
-/* api.login(session => {
-  console.log("got login");
-  api.user(user => {
-    console.log("got profile");
-    api.hubs(hubs => {
-      api.hub(hubs[0].name, hub => {
-        console.log("got hub!");
-        api.backends(backends => {
-          console.log("got backends");
-          api.backend(backends[0].name, backend =>  {
-            console.log("got backend");
-            api.calibration('ibmq_essex', calibration =>  {
-              console.log("got calibration");
-              api.parameters('ibmq_essex', parameters =>  {
-                console.log("got parameters");
-                api.jobs(jobs =>  {
-                  console.log("got jobs");
-                });
-              });
-            });
-          });
-        });
-      });
-    }); 
-  });
-}); */
+  .then(code => {
+    console.log("got code");
+    const q = `
+      OPENQASM 2.0;\ninclude "qelib1.inc";\n\nqreg q[5];\ncreg c[5];\n\nh q[0];\nh q[1];\nh q[2];\nh q[3];\nh q[4];\nx q[0];\nx q[1];\nz q[2];\ny q[3];\ncx q[0],q[1];\ncx q[3],q[4];\ncx q[2],q[3];\nmeasure q[0] -> c[0];\nmeasure q[1] -> c[1];\nmeasure q[2] -> c[2];\nmeasure q[3] -> c[3];\nmeasure q[4] -> c[4];
+    `;
+    const backend = `ibmq_qasm_simulator`;
+    const qObj = {
+      'schema_version': '1.1.0',
+      'config': {
+        "n_qubits": 5,
+        "shots": 1024,
+        "memory": false,
+        "parameter_binds": [],
+        "memory_slots": 5
+      },
+      "experiments": [
+        {
+            "instructions": [
+                {
+                    "qubits": [
+                        3
+                    ],
+                    "name": "h"
+                },
+                {
+                    "qubits": [
+                        3
+                    ],
+                    "name": "y"
+                },
+                {
+                    "qubits": [
+                        1
+                    ],
+                    "name": "h"
+                },
+                {
+                    "qubits": [
+                        1
+                    ],
+                    "name": "x"
+                },
+                {
+                    "qubits": [
+                        4
+                    ],
+                    "name": "h"
+                },
+                {
+                    "qubits": [
+                        3,
+                        4
+                    ],
+                    "name": "cx"
+                },
+                {
+                    "qubits": [
+                        4
+                    ],
+                    "memory": [
+                        4
+                    ],
+                    "name": "measure"
+                },
+                {
+                    "qubits": [
+                        0
+                    ],
+                    "name": "h"
+                },
+                {
+                    "qubits": [
+                        0
+                    ],
+                    "name": "x"
+                },
+                {
+                    "qubits": [
+                        0,
+                        1
+                    ],
+                    "name": "cx"
+                },
+                {
+                    "qubits": [
+                        0
+                    ],
+                    "memory": [
+                        0
+                    ],
+                    "name": "measure"
+                },
+                {
+                    "qubits": [
+                        1
+                    ],
+                    "memory": [
+                        1
+                    ],
+                    "name": "measure"
+                },
+                {
+                    "qubits": [
+                        2
+                    ],
+                    "name": "h"
+                },
+                {
+                    "qubits": [
+                        2
+                    ],
+                    "name": "z"
+                },
+                {
+                    "qubits": [
+                        2,
+                        3
+                    ],
+                    "name": "cx"
+                },
+                {
+                    "qubits": [
+                        3
+                    ],
+                    "memory": [
+                        3
+                    ],
+                    "name": "measure"
+                },
+                {
+                    "qubits": [
+                        2
+                    ],
+                    "memory": [
+                        2
+                    ],
+                    "name": "measure"
+                }
+            ],
+            "config": {
+                "n_qubits": 5,
+                "memory_slots": 5
+            },
+            "header": {
+                "clbit_labels": [
+                    [
+                        "c",
+                        0
+                    ],
+                    [
+                        "c",
+                        1
+                    ],
+                    [
+                        "c",
+                        2
+                    ],
+                    [
+                        "c",
+                        3
+                    ],
+                    [
+                        "c",
+                        4
+                    ]
+                ],
+                "memory_slots": 5,
+                "qubit_labels": [
+                    [
+                        "q",
+                        0
+                    ],
+                    [
+                        "q",
+                        1
+                    ],
+                    [
+                        "q",
+                        2
+                    ],
+                    [
+                        "q",
+                        3
+                    ],
+                    [
+                        "q",
+                        4
+                    ]
+                ],
+                "name": "circuit1260",
+                "n_qubits": 5,
+                "qreg_sizes": [
+                    [
+                        "q",
+                        5
+                    ]
+                ],
+                "creg_sizes": [
+                    [
+                        "c",
+                        5
+                    ]
+                ]
+            }
+        }
+    ],
+      'header': {},
+      'type': 'QASM'
+    };
+    const newJob = {  
+      'qObj': qObj,
+      'backend': {'name': backend},
+      'shots': 1024,
+      'name': "xjob_" + new Date()
+    }
+    return api.submitJob(newJob, () => console.log("job submitted"));
+  })
+  .then(jobSubmission => console.log(jobSubmission));
